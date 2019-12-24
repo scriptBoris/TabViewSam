1851379da0c027e3e46e41f69b04ad954593ba74
Boris Krit <scriptboris@gmail.com>
2019-12-24 13:48:38 +0800

    cr: Add static method for initialized

---
+++ Lib/TabInit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TabViewSam
+{
+    public static class TabInit
+    {
+        /// <summary>
+        /// For initialization in PCL part Xamarin.Forms
+        /// </summary>
+        public static void Init()
+        {
+        }
+    }
+}
